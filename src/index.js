6a3c9a25dff391f227ad7df1b612ca012c3584a5
skybird23333 <2603003199a@gmail.com>
2021-10-31 13:28:33 +0000

    b

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
-var port = 2333
 
-if(process.argv[2]) {
-    port = 6969 //dev mode
-}
-
-else {
-    app.use('/', express.static("../dist")); //not dev mode
-}
-
+app.use('/', express.static("../dist")); //not dev mode
+app.get('*', (req, res) => res.sendFile('/dist/index.html', {
+    root: path.join(process.cwd())
+}));
 
 app.get('/api*', (req, res) => {
     res.send('hi')
 })
 
 // GLOBAL ENDPOINTS
-app.get('/api/v') //version
-
-app.get('/api/f/:file') //get file info
-app.get('/api/f/:file/download') //download file(latest version)
-
-app.get('/api/f/:file/stats') //get file global stat
-
-app.get('/api/f/:file/v/:version/') //get info of a version
-app.get('/api/f/:file/v/:version/download') //download version
-app.get('/api/f/:file/v/:version/stats') //get file global stat
+//app.get('/api/v') //version
+//
+//app.get('/api/f/:file') //get file info
+//app.get('/api/f/:file/download') //download file(latest version)
+//
+//app.get('/api/f/:file/stats') //get file global stat
+//
+//app.get('/api/f/:file/v/:version/') //get info of a version
+//app.get('/api/f/:file/v/:version/download') //download version
+//app.get('/api/f/:file/v/:version/stats') //get file global stat
 
 // ADMIN ENDPOINTS
 
 
-app.post('/api/f/:file') //
+//app.post('/api/f/:file') //
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+app.listen(2333, () => {
+  console.log(`App listening at http://localhost:2333`)
 })
