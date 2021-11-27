7bebe757a207ddcf774b1a12187c5fb78b4ca317
skybird23333 <2603003199a@gmail.com>
2021-11-27 12:36:03 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,78 +1,26 @@
 (async () => {
   const express = require('express')
-  const apiRouter = require('./api/index')
   const showdown = require('showdown')
   require('dotenv').config()
-  require('./discord/index')
+  
   const discordService = require('./discord/index')
   const databaseService = require('./database/index')
-
+  
   process.on('uncaughtException', (err) => { console.error(err) })
   process.on('unhandledRejection', (err) => { console.error(err) })
-
+  
   const app = express()
   const logger = require('./logger/logger')
   const converter = new showdown.Converter()
-
+  
   module.exports = {
     logger,
     app
   }
-
+  
   app.set('view engine', 'ejs');
   app.use(express.json())
-
-
-  app.use('/api', apiRouter)
-  logger.info('API Endpoints registered')
-
-  const post = require('./testpost')
-
-  app.get('/test', (req, res) => {
-    post.content = converter.makeHtml(post.content)
-    res.render('post', { post })
-  })
-
-  app.get('/search', (req, res) => {
-    res.render('search', {
-      query: decodeURI(req.query.query)
-    })
-  })
-
-  app.get('/feedback', (req, res) => {
-    if (req.query.message && req.query.message.length > 0 && req.query.email && req.query.email.length > 0) {
-      discordService.postFeedbackMessage(req.query.message, req.query.email)
-      res.render('feedback', {
-        feedback: {
-          message: "amogus"
-        }
-      })
-    } else {
-      res.render('feedback', { feedback: null })
-    }
-  })
-
-  app.use('/', express.static('public'))
-
-  app.get('/', async (req, res) => {
-    const presence = await discordService.getSkybirdPresence()
-    res.render('index', {
-      presence
-    })
-  })
-
-  app.get('/*', (req, res) => {
-    res.render('notFound')
-  })
-
-  app.use(function errorHandler(err, req, res, next) {
-    if (res.headersSent) {
-      return next(err);
-    }
-    res.status(500);
-    res.render('error', { error: err });
-  })
-
+  
   await discordService.initClient().then(client => {
     logger.info(`DISCORD: Logged in as ${client.user.tag}`);
   })
@@ -80,6 +28,14 @@
   await databaseService.initClient().then(() => {
     logger.info(`MONGODB: Logged into database`);
   })
+
+  const apiRouter = require('./api/index')
+  const Router = require('./routes/index')
+  
+  app.use('/api', apiRouter)
+  logger.info('API Endpoints registered')
+  app.use('/', Router)
+  logger.info('Routes registered')
 
   app.listen(2333 || process.env.PORT, () => {
     logger.done(`App is running at http://localhost:${2333 || process.env.PORT}`)
---
+++ src/routes/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const Router = new express.Router()
+
+const discordService = require('../discord/index')
+const databaseService = require('../database/index')
+
+const post = require('./testpost')
+
+Router.get('/test', (req, res) => {
+    post.content = converter.makeHtml(post.content)
+    res.render('post', { post })
+})
+
+Router.get('/search', (req, res) => {
+    res.render('search', {
+        query: decodeURI(req.query.query)
+    })
+})
+
+Router.get('/feedback', (req, res) => {
+    if (req.query.message && req.query.message.length > 0 && req.query.email && req.query.email.length > 0) {
+        discordService.postFeedbackMessage(req.query.message, req.query.email)
+        res.render('feedback', {
+            feedback: {
+                message: "amogus"
+            }
+        })
+    } else {
+        res.render('feedback', { feedback: null })
+    }
+})
+
+Router.use('/', express.static('public'))
+
+Router.get('/', async (req, res) => {
+    const presence = await discordService.getSkybirdPresence()
+    res.render('index', {
+        presence
+    })
+})
+
+Router.get('/*', (req, res) => {
+    res.render('notFound')
+})
+
+Router.use(function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.render('error', { error: err });
+})
+module.exports = Router
