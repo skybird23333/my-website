e3ff31e8e4036bc8f42e0e47d288f6cc5eab49cc
2603003199a <58811224+skybird23333@users.noreply.github.com>
2022-07-24 01:37:54 +0800

    Fix: Sidebar generation breaks when there are no headings

--- src/util/generate-post-content.js
+++ src/util/generate-post-content.js
@@ -5,17 +5,21 @@
  * @returns {object[]} An array of contents object, please see the function itself
  */
 module.exports = function generatePostContent(content) {
-    return content.match(/\n(?=# ).+/g) //match ONLY the big headings
-           .map(text => {
-                return text.slice(3) //remove \n, # and space
-           })
-           .map(text => {
-               console.log(text)
-                return {
-                   text: text,
-                   id: text.replace(/\s/g, '-') //replace spaces with dashes
+
+    const match = content.match(/\n(?=# ).+/g)
+
+    return match ? match //match ONLY the big headings
+        .map(text => {
+            return text.slice(3) //remove \n, # and space
+        })
+        .map(text => {
+            console.log(text)
+            return {
+                text: text,
+                id: text.replace(/\s/g, '-') //replace spaces with dashes
                     .replace(/([^a-zA-Z-])/g, '') //remove non-alphanumeric characters
                     .toLowerCase() //make lowercase
-               }
-           })
+            }
+        })
+        : []
 } 
